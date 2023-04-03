0ce7dc03d024ec06df40fe5af0b874119b1bcf7b
Brannigan CHATEAU <brannigan.pro@gmail.com>
2023-04-03 11:35:20 +0200

    Dev profiles

--- src/main/java/fr/dawan/training/TrainingApplication.java
+++ src/main/java/fr/dawan/training/TrainingApplication.java
@@ -3,6 +3,8 @@ package fr.dawan.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Point d'entrée
+
 @SpringBootApplication
 public class TrainingApplication {
 
