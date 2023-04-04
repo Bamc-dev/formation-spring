7e5d8189b459666e969e1762e94a3f8f6fc8e634
Brannigan CHATEAU <brannigan.pro@gmail.com>
2023-04-04 13:59:09 +0200

    Début bdd

--- src/main/java/fr/dawan/training/TrainingApplication.java
+++ src/main/java/fr/dawan/training/TrainingApplication.java
@@ -1,7 +1,14 @@
 package fr.dawan.training;
 
+import fr.dawan.training.interceptors.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //Point d'entrée
 
@@ -9,8 +16,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Si changement de package : @ComponentScan()
 public class TrainingApplication {
 
+	@Value("${cors.allowed-urls}")
+	private String allowedUrls;
+	@Autowired
+	private MyInterceptor myInterceptor;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer mvcConfigurer() {
+		return new WebMvcConfigurer() {
 
-}
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/admin/*")
+//					.allowedOrigins("dawan.fr")
+//					.allowedMethods("GET","POST");
+
+				//On autorise les domaines renseignés dans allowedUrls
+				//à accéder à n'importe quel url de mon service avec toutes les méthodes
+				registry.addMapping("/**")
+						.allowedOrigins(allowedUrls.split(","))
+						.allowedMethods("GET","POST","PUT","DELETE","PATCH", "OPTIONS");
+
+			}
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(myInterceptor);
+			}
+
+
+		};
+	}
+	}
---
+++ src/main/java/fr/dawan/training/entities/Product.java
@@ -0,0 +1,54 @@
+package fr.dawan.training.entities;
+
+import java.util.Objects;
+
+public class Product {
+    private long id;
+    private String description;
+    private double price;
+    private String imagePath;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+}
